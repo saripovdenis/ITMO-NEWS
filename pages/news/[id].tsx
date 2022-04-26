fae878f84a1b6cb0a8bcbaac4f850f25a9277a24
Denis Saripov <bblankbboi@gmail.com>
2022-04-26 17:04:30 +0300

    Добавлен выброс ошибки при обновлении страницы новости

--- pages/news/[id].tsx
+++ pages/news/[id].tsx
@@ -11,13 +11,12 @@ const News: React.FC = () => {
   const router = useRouter();
   const descRef = useRef<HTMLDivElement>(null);
   const { id } = router.query;
-  const { title, description } = useAppSelector(
-    ({ news }: RootState) =>
-      news.news.find((obj: NewsItem) => obj.id === Number(id)) ?? {
-        title: "title",
-        description: "description",
-      }
-  );
+  const { title, description } = useAppSelector(({ news }: RootState) => {
+    const newsItem = news.news.find((obj: NewsItem) => obj.id === Number(id));
+    if (newsItem) return newsItem;
+    // При возможности отправлялся бы запрос по id определённой новости
+    throw new Error("Ошибка в получении данных");
+  });
 
   useEffect(() => {
     if (descRef.current) descRef.current.innerHTML = description;

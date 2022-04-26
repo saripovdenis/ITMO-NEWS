c9eb232ecbfccc454abc7d6ac71b312963d4a1ff
Denis Saripov <bblankbboi@gmail.com>
2022-04-26 03:14:35 +0300

    Добавлено получение данных с api и сохранение в стор

--- common/containers/HomeContent/HomeContent.tsx
+++ common/containers/HomeContent/HomeContent.tsx
@@ -2,17 +2,31 @@ import React, { useEffect } from "react";
 import styles from "./HomeContent.module.css";
 import { Card } from "../../lib";
 import { apiService } from "../../utils";
-import { useToggle } from "../../hooks";
-import { useSelector } from "react-redux";
+import { useToggle, useAppSelector, useAppDispatch } from "../../hooks";
 import { RootState } from "../../../store";
+import { setNews } from "../../../store/news";
 
 const HomeContent: React.FC = ({}) => {
+  const dispatch = useAppDispatch();
   const [isLoading, toggleIsLoading] = useToggle(false);
-  const news = useSelector(({ news }: RootState) => news.news);
+  const news = useAppSelector(({ news }: RootState) => news.news);
 
   useEffect(() => {
     toggleIsLoading();
-    apiService.get("").then(console.log);
+    apiService.get("").then(({ data }) =>
+      dispatch(
+        setNews(
+          data.news.map(({ id, date, title, image_big, image_small }: any) => ({
+            id,
+            date,
+            title,
+            image_big,
+            image_small,
+            description: "description",
+          }))
+        )
+      )
+    );
     toggleIsLoading();
   }, []);
 
